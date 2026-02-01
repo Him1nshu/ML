0af56bea4c53928949c6cc148d3a6c83e89b8ab8
Him1nshu <astng888@gmail.com>
2026-02-01 22:40:34 +0530

    final working commit

---
+++ webscrp/README.md
@@ -0,0 +1,20 @@
+# web scraper
+# this is a project which i have taken from roadmap.sh
+## https://roadmap.sh/projects/job-listings-scraper
+# the objective was to
+<br>
+Extract the following fields for each job posting:
+
+Job title
+
+Company name
+
+Location
+
+Job detail page URL
+
+Store the results in a CSV file
+
+Use clean, readable Python code
+
+Handle simple edge cases (e.g., missing fields)
--- webscrp/main.py
+++ webscrp/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 
 url = "https://realpython.github.io/fake-jobs/"
@@ -14,17 +15,35 @@ j3=soup.find_all("footer", class_="card-footer")
 j2=soup.find_all("div", class_="content") 
 j1=soup.find_all("div", class_="media-content")
 
+
+titles = []
+companies = []
+locations = []
+links = []
+
 for j in j1:
-    ttile= j.find("h2", class_="title is-5").text
-    comp = j.find("h3", class_="subtitle is-6 company").text
-    print(ttile,"@",comp)
+    title_tag = j.find("h2", class_="title is-5")
+    comp_tag = j.find("h3", class_="subtitle is-6 company")
+    title = title_tag.get_text(strip=True) if title_tag else "N/A"
+    comp = comp_tag.get_text(strip=True) if comp_tag else "N/A"
+    #print(title, "@", comp)
+    titles.append(title)
+    companies.append(comp)
 
 for j in j2:
-    loc=j.find("p",class_="location").text
-    print(loc)
+    loc_tag = j.find("p", class_="location")
+    loc = loc_tag.get_text(strip=True) if loc_tag else "Location not found"
+    #print(loc)
+    locations.append(loc)
 
 for j in j3:
-    a=j.find("a")
-    link=a.get("href")
-    print(link)
+    a = j.find("a")
+    link = a.get("href") if a else "No link available"
+    #print(link)
+    links.append(link)
 
+with open("jobs.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["title", "company", "location"," details"])
+    for t, c,l,d in zip(titles,companies,locations,links):
+        writer.writerow([t,c,l,d])
