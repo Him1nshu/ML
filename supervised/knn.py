2330e5b32233e23461fdede7705f3bf2c9007b01
Him1nshu <astng888@gmail.com>
2026-03-27 22:42:31 +0530

    updated knn.py

--- supervised/knn.py
+++ supervised/knn.py
@@ -1,9 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 
-x=[1,2,3,4]
-y=[6,2,3,6]
-cl=[0,1,1,0]
+x = [1, 2, 3, 4]
+y = [6, 2, 3, 6]
+cl = [0, 1, 1, 0]
 
-plt.scatter(x,y,c=cl)
+plt.scatter(x, y, c=cl)
+plt.show()
+
+data = list(zip(x, y))
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(data, cl)
+
+nx, ny = 5, 5
+npp = [[nx, ny]]
+
+plt.scatter(x + [nx], y + [ny], c=cl + [knn.predict(npp)])
 plt.show()
